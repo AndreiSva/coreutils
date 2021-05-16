b933717054176faae7d31896205b16ab3dcaf17e
AndreiSva <AndreiSva2023@gmail.com>
2021-05-16 11:41:23 -0700

    fixed bug and added diplay lines in ls

--- src/ls.c
+++ src/ls.c
@@ -10,10 +10,11 @@ int files_read = 0;
 
 bool list_all;
 bool color;
+int display_per_line = 12;
 int main(int argc, char** argv) {
 	char* folders[1000];
 	if (argc == 1 || argv[argc - 1][0] == '-') {
-		target = ".";
+		target = "./";
 	} else {
 		target = argv[argc - 1];
 	}
@@ -26,6 +27,9 @@ int main(int argc, char** argv) {
 					break;
 				case 'c':
 					color = true;
+					break;
+				case 'l':
+					display_per_line = argv[i][2] - '0';
 					break;
 			}
 		}
@@ -42,27 +46,40 @@ int main(int argc, char** argv) {
 		}
 	}
 	
+	int folders_printed = 0;
 	for (char c = 34; c < 127; c++) {
 		for (int i = 0; i < files_read; i++) {
 			if (folders[i][0] == c) {
 				if (!list_all) {
 					if (folders[i][0] == '.') {
-							continue;
+						continue;
 					}
 				}
+				
+				char* path_string = malloc(512);	
+				strcpy(path_string, target);
+				strcat(path_string, folders[i]);
 
 				if (color) {
 					struct stat sb;
 					if (!strcmp(folders[i], ".") || !strcmp(folders[i], "..")) {
 						printf("\033[0;34m");
-					} else if (stat(folders[i], &sb) == 0 && sb.st_mode & S_IFDIR) {
+					} else if (stat(path_string, &sb) == 0 && sb.st_mode & S_IFDIR) {
 						printf("\033[1m\033[34m");
-					} else if (stat(folders[i], &sb) == 0 && sb.st_mode & S_IXUSR) {
+					} else if (stat(path_string, &sb) == 0 && sb.st_mode & S_IXUSR) {
 						printf("\033[1m\033[32m");
 					}
 				}
 
+				free(path_string);
+
 				printf("%s  \033[0m", folders[i]);
+				folders_printed++;
+
+				if (folders_printed >= display_per_line) {
+					printf("\n");
+					folders_printed = 0;
+				}
 			}
 		}
 	}
